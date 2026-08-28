8b72e9b0d87adc69f55d8559b9b6594dc3afbef0
IssaItani-iri00 <IssaItani-iri00@users.noreply.github.com>
2026-08-28 23:37:35 +0300

    removed the mock service layer

--- src/services/mock/mockBalanceService.ts
+++
@@ -1,9 +0,0 @@
-import type { BalanceService } from "../balanceService";
-import { clone, db, delay } from "./database";
-
-export const mockBalanceService: BalanceService = {
-  async getBalances() {
-    await delay();
-    return clone(db.balances);
-  },
-};
--- src/services/mock/mockBuildingService.ts
+++
@@ -1,9 +0,0 @@
-import type { BuildingService } from "../buildingService";
-import { clone, db, delay } from "./database";
-
-export const mockBuildingService: BuildingService = {
-  async getBuildings() {
-    await delay();
-    return clone(db.buildings);
-  },
-};
